158933106cfe32465084ace05dca8d6e8ed82ddd
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-17 18:37:06 -0400

    create controllers, added file views, added bcrypt gemfile,user forms, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+end
+
+Rails.application.routes.draw do
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816204200) do
+ActiveRecord::Schema.define(version: 20160817221723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20160816204200) do
     t.string   "web_link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
