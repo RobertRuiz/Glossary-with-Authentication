5ecf507e776e7250661f033e3328a99373660ca4
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-18 13:01:13 -0400

    homepage functional

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @terms = Term.all.order(created_at: :desc).take(5)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   resources :categories
   resources :terms
+  resources :users
+  resources :session
   get 'welcome/index'
 
   root 'welcome#index'
