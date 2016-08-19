d9862b7abb5f255b808f5f0cca5c84ebd3dc87d2
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-19 17:09:52 -0400

    Signup form now fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,11 @@ Rails.application.routes.draw do
   # root + 'name of controller= welcome #name of the action'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-end
 
-Rails.application.routes.draw do
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   get '/logout' => 'session#destroy'
+
 end
